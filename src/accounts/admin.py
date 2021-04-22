3a0ef538dc0b4054cbf7b95104992907cdba0bc2
Oytizzo <oytizzo@gmail.com>
2021-04-22 18:07:39 +0600

    Custom user and admin created

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+class UserAdmin(BaseUserAdmin):
+
+
+admin.site.unregister(Group)
